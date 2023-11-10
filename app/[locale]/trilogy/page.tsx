4b2a3540ee613c83b37a09f72a239b50b828746a
john-moonbox <john.chi@moonbox.com>
2023-11-10 17:39:18 +0800

    fix(trilogy): fix style

--- app/[locale]/trilogy/page.tsx
+++ app/[locale]/trilogy/page.tsx
@@ -8,49 +8,49 @@ export default function Trilogy() {
   return (
     <div className="flex bg-black">
       <Header />
-      <div className="mt-[75px] flex w-full flex-col items-center pb-[60px] lg:mt-[160px]">
-        <div className="flex flex-col px-[15px] sm:h-[337.5px] sm:w-[600px] xl:h-[675px] xl:w-[1200px]">
+      <div className="mt-[92px] flex w-full flex-col items-center pb-[80px] lg:mt-[200px] lg:pb-[150px]">
+        <div className="mt-[40px] flex w-min flex-col">
           <iframe
-            className="h-full w-full"
-            src="https://www.youtube.com/embed/euXWjps13aw?si=M3AaikWtswWke_JW"
+            className="h-[180px] w-[320px] md:h-[360px] md:w-[640px] lg:h-[432px] lg:w-[768px] xl:h-[675px] xl:w-[1200px]"
+            src="https://www.youtube.com/embed/euXWjps13aw?listType=user_uploads"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           />
-          <h3 className="font-Inter mb-[5px] mt-[15px] text-[18px] font-semibold leading-[24px] text-white">
+          <h3 className="font-Inter mt-[20px] text-[21px] font-semibold leading-[24px] text-white lg:mt-[40px] lg:text-[30px]">
             {t("chapter_one")}
           </h3>
-          <span className="font-Inter text-[16px] font-semibold leading-[24px] text-white">
+          <span className="font-Inter mt-[10px] text-[16px] font-semibold leading-[24px] text-white lg:mt-[20px] lg:text-[21px]">
             {t("chapter_one_description")}
           </span>
         </div>
-        <div className="mt-[60px] flex flex-col px-[15px] sm:h-[337.5px] sm:w-[600px] xl:h-[675px] xl:w-[1200px]">
+        <div className="mt-[40px] flex w-min flex-col">
           <iframe
-            className="h-full w-full"
-            src="https://www.youtube.com/embed/r1D8ZSzm87U?si=Y24r2o2U0p_5y5l3"
+            className="h-[180px] w-[320px] md:h-[360px] md:w-[640px] lg:h-[432px] lg:w-[768px] xl:h-[675px] xl:w-[1200px]"
+            src="https://www.youtube.com/embed/r1D8ZSzm87U?listType=user_uploads"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           />
-          <h3 className="font-Inter mb-[5px] mt-[15px] text-[18px] font-semibold leading-[24px] text-white">
+          <h3 className="font-Inter mt-[20px] text-[21px] font-semibold leading-[24px] text-white lg:mt-[40px] lg:text-[30px]">
             {t("chapter_two")}
           </h3>
-          <span className="font-Inter text-[16px] font-semibold leading-[24px] text-white">
+          <span className="font-Inter mt-[10px] text-[16px] font-semibold leading-[24px] text-white lg:mt-[20px] lg:text-[21px]">
             {t("chapter_two_description")}
           </span>
         </div>
-        <div className="mt-[60px] flex flex-col px-[15px] sm:h-[337.5px] sm:w-[600px] xl:h-[675px] xl:w-[1200px]">
+        <div className="mt-[40px] flex w-min flex-col">
           <iframe
-            className="h-full w-full"
-            src="https://www.youtube.com/embed/euXWjps13aw?si=M3AaikWtswWke_JW"
+            className="h-[180px] w-[320px] md:h-[360px] md:w-[640px] lg:h-[432px] lg:w-[768px] xl:h-[675px] xl:w-[1200px]"
+            src="https://www.youtube.com/embed/euXWjps13aw?listType=user_uploads"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           />
-          <h3 className="font-Inter mb-[5px] mt-[15px] text-[18px] font-semibold leading-[24px] text-white">
+          <h3 className="font-Inter mt-[20px] text-[21px] font-semibold leading-[24px] text-white lg:mt-[40px] lg:text-[30px]">
             {t("chapter_three")}
           </h3>
-          <span className="font-Inter text-[16px] font-semibold leading-[24px] text-white">
+          <span className="font-Inter mt-[10px] text-[16px] font-semibold leading-[24px] text-white lg:mt-[20px] lg:text-[21px]">
             {t("chapter_three_description")}
           </span>
         </div>

d31070d582dd120976d4a16e2d2183e17e3decb9
john-moonbox <john.chi@moonbox.com>
2023-11-20 13:39:42 +0800

    update

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,25 +1,23 @@
-"use client"
+"use client";
 // import { ConnectButton } from '@rainbow-me/rainbowkit'
-import 'core-js/features/object/has-own';
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
-import Head from 'next/head'
-import { useTranslations } from 'next-intl'
-import Header from '@/components/Header'
-import FirstSection from './home_section'
+import "core-js/features/object/has-own";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import { useTranslations } from "next-intl";
+import Header from "@/components/Header";
+import FirstSection from "./home_section";
 import {
   FullpageContainer,
   FullpageSection,
-} from '@shinyongjun/react-fullpage';
-import '@shinyongjun/react-fullpage/css';
-import Typed from 'typed.js'
+} from "@shinyongjun/react-fullpage";
+import "@shinyongjun/react-fullpage/css";
+import Typed from "typed.js";
 
 export default function Home() {
-  const t = useTranslations('Home');
+  const t = useTranslations("Home");
 
   const chatListBottomRef = useRef<HTMLEmbedElement>(null);
-
-  const secondPageStoryIntroduce = useRef<HTMLDivElement>(null);
 
   const [playingMedia, setPlayingMedia] = useState<boolean>(false);
 
@@ -29,40 +27,21 @@ export default function Home() {
   const [isTypedSecondPage, setTypedSecondPage] = useState<boolean>(false);
 
   useEffect(() => {
-    chatListBottomRef.current?.scrollTo({ top: chatListBottomRef.current?.scrollHeight, behavior: "smooth" })
-  }, [chatListBottomRef.current?.scrollHeight])
-
-  useEffect(() => {
-    if (activeIndex == 1 && !isTypedSecondPage) {
-      var typed = new Typed(secondPageStoryIntroduce.current, {
-        strings: [t('story_introduce')],
-        typeSpeed: 20,
-      })
-      setTypedSecondPage(true)
-    }
-  }, [activeIndex])
+    chatListBottomRef.current?.scrollTo({
+      top: chatListBottomRef.current?.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [chatListBottomRef.current?.scrollHeight]);
 
   return (
-    <div className=' bg-gray-600'>
+    <div className=" bg-gray-600">
       <Head>
         <title>Moonbox</title>
         <meta name="description" content="Bring life to NFTs" />
-        <meta
-          property="og:title"
-          content="Moonbox"
-        />
-        <meta
-          property="og:description"
-          content="Bring life to NFTs"
-        />
-        <meta
-          property="og:image"
-          content="/home_video_cover.png"
-        />
-        <meta
-          property="og:url"
-          content="https://moonbox.com"
-        />
+        <meta property="og:title" content="Moonbox" />
+        <meta property="og:description" content="Bring life to NFTs" />
+        <meta property="og:image" content="/home_video_cover.png" />
+        <meta property="og:url" content="https://moonbox.com" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
@@ -75,7 +54,7 @@ export default function Home() {
         transitionDuration={600}
         onBeforeChange={(before, after) => {
           if (showChatModal) {
-            setActiveIndex(0)
+            setActiveIndex(0);
           }
         }}
       >
@@ -84,24 +63,45 @@ export default function Home() {
             playingMedia={playingMedia}
             setPlayingMedia={setPlayingMedia}
             showChatModal={showChatModal}
-            setShowChatModal={setShowChatModal} />
+            setShowChatModal={setShowChatModal}
+          />
         </FullpageSection>
 
         <FullpageSection>
-          <div className=" w-full h-screen bg-[#151515] bg-contain bg-[80%_80%] bg-[url('/bg-section2.png')] relative flex items-center">
-            <div className=' absolute scale-[0.3] top-[8%] sm:top-[130px] left-[-5%] sm:left-[170px] w-[186px] h-[128px] sm:scale-50 lg:scale-[0.6] 2xl:scale-[0.7] 3xl:scale-[0.8] 4xl:scale-[0.9]'>
-              <Image src="/spaceship.png" alt='spaceship' fill />
+          <div className="flex h-auto w-full flex-col items-center bg-[#151515] bg-[url('/bg-section2.png')] bg-contain bg-[80%_80%]">
+            <div className="flex flex-col items-center pt-[230px] pb-[160px]">
+              <span className="text-[24px] font-bold leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[60px] 3xl:leading-[60px]">
+                About Nobody
+              </span>
+              <span className="mt-[30px] max-w-[1093px] text-center text-[24px] font-medium leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px]">
+                Welcome to moonbox , a place that celebrates connection, unity,
+                and togetherness.
+              </span>
+              <span className="mt-[30px] max-w-[1117px] text-center text-[24px] font-medium leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px]">
+                Our dedicated team is committed to creating value and providing
+                unforgettable experiences for our NFT holders. Join us on this
+                exciting journey as we continue to expand and evolve in the
+                world of digital collectibles.
+              </span>
             </div>
-            <div className=' absolute sm:bottom-[122px] bottom-[8%] right-[3%] sm:right-[171px] w-[125px] h-[139px] scale-[0.50] sm:scale-50  2xl:scale-75 3xl:scale-100'>
-              <Image src="/rocket.png" alt='rocket' fill />
-            </div>
-            <div className=' m-auto max-w-[80%] lg:ml-[170px] relative z-10 '>
-              <p className=' text-[24px] leading-[36px] lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px] font-medium text-white max-w-[1093px]' ref={secondPageStoryIntroduce}></p>
+            <div className="flex flex-col items-center py-[160px]">
+              <span className="text-[24px] font-bold leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[60px] 3xl:leading-[60px]">
+                Auction Schedule
+              </span>
+              <div className="flex">
+                <div className="flex flex-col">
+                  <span className="mt-[30px] max-w-[1093px] text-center text-[24px] font-medium leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px]">
+                    Invite registration
+                  </span>
+                  <span className="mt-[30px] max-w-[1117px] text-center text-[24px] font-medium leading-[36px] text-white lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px]">
+                    12/15-12/30 24:00
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </FullpageSection>
-
       </FullpageContainer>
     </div>
-  )
+  );
 }

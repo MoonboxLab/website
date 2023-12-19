0aedf7c6a110bf54f1bbcbb23a6cd8a442dc901b
john-moonbox <john.chi@moonbox.com>
2023-12-20 01:12:15 +0800

    update

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,108 +1,172 @@
-"use client"
-// import { ConnectButton } from '@rainbow-me/rainbowkit'
-import 'core-js/features/object/has-own';
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
-import Head from 'next/head'
-import { useTranslations } from 'next-intl'
-import Header from '@/components/Header'
-import FirstSection from './home_section'
+"use client";
+
+import "core-js/features/object/has-own";
+import Image from "next/image";
+import { useSize } from "ahooks";
+import { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import { useTranslations } from "next-intl";
+import Header from "@/components/Header";
 import {
   FullpageContainer,
   FullpageSection,
-} from '@shinyongjun/react-fullpage';
-import '@shinyongjun/react-fullpage/css';
-import Typed from 'typed.js'
+} from "@shinyongjun/react-fullpage";
+import "@shinyongjun/react-fullpage/css";
+import Footer from "@/components/ConnectWallet";
 
 export default function Home() {
-  const t = useTranslations('Home');
+  const t = useTranslations("Home");
+
+  const mediaSize = useSize(document.querySelector("body"));
 
   const chatListBottomRef = useRef<HTMLEmbedElement>(null);
 
   const secondPageStoryIntroduce = useRef<HTMLDivElement>(null);
 
-  const [playingMedia, setPlayingMedia] = useState<boolean>(false);
-
-  const [showChatModal, setShowChatModal] = useState<boolean>(false);
-
   const [activeIndex, setActiveIndex] = useState<number>(0);
-  const [isTypedSecondPage, setTypedSecondPage] = useState<boolean>(false);
 
   useEffect(() => {
-    chatListBottomRef.current?.scrollTo({ top: chatListBottomRef.current?.scrollHeight, behavior: "smooth" })
-  }, [chatListBottomRef.current?.scrollHeight])
-
-  useEffect(() => {
-    if (activeIndex == 1 && !isTypedSecondPage) {
-      var typed = new Typed(secondPageStoryIntroduce.current, {
-        strings: [t('story_introduce')],
-        typeSpeed: 20,
-      })
-      setTypedSecondPage(true)
-    }
-  }, [activeIndex])
+    chatListBottomRef.current?.scrollTo({
+      top: chatListBottomRef.current?.scrollHeight,
+      behavior: "smooth",
+    });
+  }, [chatListBottomRef.current?.scrollHeight]);
 
   return (
-    <div className=' bg-gray-600'>
+    <div className=" bg-gray-600">
       <Head>
         <title>Moonbox</title>
         <meta name="description" content="Bring life to NFTs" />
-        <meta
-          property="og:title"
-          content="Moonbox"
-        />
-        <meta
-          property="og:description"
-          content="Bring life to NFTs"
-        />
-        <meta
-          property="og:image"
-          content="/open-graph.png"
-        />
-        <meta
-          property="og:url"
-          content="https://moonbox.com"
-        />
+        <meta property="og:title" content="Moonbox" />
+        <meta property="og:description" content="Bring life to NFTs" />
+        <meta property="og:image" content="/open-graph.png" />
+        <meta property="og:url" content="https://moonbox.com" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://moonbox.com/open-graph.jpg"></meta>
+        <meta
+          name="twitter:image"
+          content="https://moonbox.com/open-graph.jpg"
+        ></meta>
       </Head>
 
       <Header />
 
-      <FullpageContainer
-        activeIndex={activeIndex}
-        setActiveIndex={setActiveIndex}
-        transitionDuration={600}
-        onBeforeChange={(before, after) => {
-          if (showChatModal) {
-            setActiveIndex(0)
-          }
-        }}
-      >
-        <FullpageSection>
-          <FirstSection
-            playingMedia={playingMedia}
-            setPlayingMedia={setPlayingMedia}
-            showChatModal={showChatModal}
-            setShowChatModal={setShowChatModal} />
-        </FullpageSection>
+      {/* PC UI */}
+      {(mediaSize?.width || 0) >= 1024 && (
+        <FullpageContainer
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+          transitionDuration={600}
+        >
+          <FullpageSection>
+            <div className="relative h-screen w-full items-center">
+              <Image src="/home_bg_mint.jpg" fill alt="mint" priority />
+              <div className="hover-btn-shadow absolute bottom-[150px] left-[30px] flex h-[80px] w-[200px] items-center justify-center rounded-[10px] border-2 border-black bg-white pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)]">
+                <Image
+                  src="/home_play_mv.png"
+                  height={64}
+                  width={64}
+                  alt="play"
+                  priority
+                />
+                <span className="ml-[16px] text-[18px] font-semibold leading-[24px] text-black">
+                  {t("nobody_nft_mv")}
+                </span>
+              </div>
+              <Footer />
+            </div>
+          </FullpageSection>
+          <FullpageSection>
+            <div className="flex w-full items-center bg-white px-[65px]">
+              <div className="h-full w-1/3 bg-white"></div>
+              <div className="flex h-full w-2/3 flex-col items-center pb-[175px] pt-[150px]">
+                <span className="text-[48px] font-bold text-black">
+                  ORIGIN STORY OF NOBODY
+                </span>
+                <div className="flex flex-col items-start">
+                  <div
+                    className="text-balck mt-[50px] max-w-[1024px] text-[24px] font-semibold leading-[36px]"
+                    dangerouslySetInnerHTML={{
+                      __html: t.raw("nobody_story_content"),
+                    }}
+                  />
+                  <div className="mt-[80px] grid grid-cols-3 items-center gap-[30px]">
+                    <div className="flex flex-col items-center">
+                      <div className="relative">
+                        <Image
+                          src="/chapter_one.jpg"
+                          width={300}
+                          height={165}
+                          alt="chapter one"
+                          priority
+                        />
+                        <Image
+                          className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+                          src="/chapter_play.png"
+                          width={64}
+                          height={64}
+                          alt="play"
+                          priority
+                        />
+                      </div>
+                      <span className="mt-[20px] text-[18px] font-semibold leading-[18px] text-black">
+                        Chapter One : Spacebus
+                      </span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <div className="relative">
+                        <Image
+                          src="/chapter_two.jpg"
+                          width={300}
+                          height={165}
+                          alt="chapter two"
+                          priority
+                        />
+                        <Image
+                          className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+                          src="/chapter_play.png"
+                          width={64}
+                          height={64}
+                          alt="play"
+                          priority
+                        />
+                      </div>
+                      <span className="mt-[20px] text-[18px] font-semibold leading-[18px] text-black">
+                        Chapter Two : Accident
+                      </span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <div className="relative">
+                        <Image
+                          src="/chapter_three.jpg"
+                          width={300}
+                          height={165}
+                          alt="chapter three"
+                          priority
+                        />
+                        <Image
+                          className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+                          src="/chapter_play.png"
+                          width={64}
+                          height={64}
+                          alt="play"
+                          priority
+                        />
+                      </div>
+                      <span className="mt-[20px] text-[18px] font-semibold leading-[18px] text-black">
+                        Chapter Three : Arrival
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Footer />
+          </FullpageSection>
+        </FullpageContainer>
+      )}
 
-        <FullpageSection>
-          <div className=" w-full h-screen bg-[#151515] bg-contain bg-[80%_80%] bg-[url('/bg-section2.png')] relative flex items-center">
-            <div className=' absolute scale-[0.3] top-[8%] sm:top-[130px] left-[-5%] sm:left-[170px] w-[186px] h-[128px] sm:scale-50 lg:scale-[0.6] 2xl:scale-[0.7] 3xl:scale-[0.8] 4xl:scale-[0.9]'>
-              <Image src="/spaceship.png" alt='spaceship' fill />
-            </div>
-            <div className=' absolute sm:bottom-[122px] bottom-[8%] right-[3%] sm:right-[171px] w-[125px] h-[139px] scale-[0.50] sm:scale-50  2xl:scale-75 3xl:scale-100'>
-              <Image src="/rocket.png" alt='rocket' fill />
-            </div>
-            <div className=' m-auto max-w-[80%] lg:ml-[170px] relative z-10 '>
-              <p className=' text-[24px] leading-[36px] lg:text-[32px] lg:leading-[42px] 3xl:text-[36px] 3xl:leading-[48px] font-medium text-white max-w-[1093px]' ref={secondPageStoryIntroduce}></p>
-            </div>
-          </div>
-        </FullpageSection>
-
-      </FullpageContainer>
+      {/* Mobile UI */}
     </div>
-  )
+  );
 }
--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,26 +1,74 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi"
+import Image from "next/image";
 
-const ConnectWallet = () => {
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 left-0 flex h-[120px] w-full items-center bg-black/80 px-[30px]">
+      <Image
+        src="/nobody_logo.png"
+        height={64}
+        width={64}
+        alt="nobody logo"
+        priority
+      />
+      <span className="ml-[10px] text-[24px] font-bold text-white">Nobody</span>
+      <div className="ml-[50px] flex">
+        <Image
+          src="/mint_progress_now.png"
+          height={48}
+          width={20}
+          alt="now"
+          priority
+        />
+        <div className="ml-[20px] flex flex-col ">
+          <span className="text-[24px] font-semibold text-yellow-300">
+            Presale
+          </span>
+          <span className="text-[18px] font-semibold text-yellow-300">
+            01/23 08:00~01/24 08:00(UTC8)
+          </span>
+        </div>
+      </div>
+      <div className="ml-[60px] flex">
+        <Image
+          src="/mint_progress_next.png"
+          height={48}
+          width={20}
+          alt="now"
+          priority
+        />
+        <div className="ml-[20px] flex flex-col ">
+          <span className="text-[24px] font-semibold text-white">
+            Public sale
+          </span>
+          <span className="text-[18px] font-semibold text-white">
+            01/24 08:00~01/25 08:00(UTC8)
+          </span>
+        </div>
+      </div>
 
-  const { connectors, connect } = useConnect()
-  const { disconnect } = useDisconnect()
-  const { address } = useAccount()
+      <div className="ml-[60px] flex">
+        <Image
+          src="/mint_progress_next.png"
+          height={48}
+          width={20}
+          alt="now"
+          priority
+        />
+        <div className="ml-[20px] flex flex-col ">
+          <span className="text-[24px] font-semibold text-white">Refund</span>
+          <span className="text-[18px] font-semibold text-white">
+            Start at 01-26 08:00(UTC8)
+          </span>
+        </div>
+      </div>
 
-  return <div>
+      <div className="hover-btn-shadow ml-auto flex h-[64px] w-[320px] items-center justify-center rounded-[10px] border-2 border-black bg-yellow-300 pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)]">
+        <span className="text-[18px] font-semibold leading-[24px] text-black">
+          Mint
+        </span>
+      </div>
+    </div>
+  );
+};
 
-    {address && <div>
-      <div>{address}</div>
-      <button onClick={() => disconnect()}>logout</button>
-    </div>}
-    {
-      connectors.map((connector, index) => {
-
-        return <button key={index} className=" p-2 m-4" onClick={() => connect({ connector })}>
-          {connector.name}
-        </button>
-      })
-    }
-  </div>
-}
-
-export default ConnectWallet
+export default Footer;

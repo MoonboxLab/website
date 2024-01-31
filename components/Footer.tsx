2131c0171bc64bc8013f649224750750e28a7ed4
D-lyw <liuyuanwang4321@gmail.com>
2024-01-31 11:35:59 +0800

    feat: hidden whitelist check button when start mint

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useCountDown } from "ahooks";
+import { MINT_START_TIME } from "@/constants/nobody_contract";
 
 type FooterProps = {
   countdown: { days: number; hours: number; minutes: number; seconds: number };
@@ -8,6 +10,8 @@ type FooterProps = {
 
 const Footer = (props: FooterProps) => {
   const t = useTranslations("Home");
+
+  const [mintStartCountdown] = useCountDown({ targetDate: MINT_START_TIME })
 
   return (
     <div className="absolute bottom-0 left-0 z-50 flex h-[88px] w-full items-center bg-black/80 px-[20px] 4xl:h-[112px] 4xl:px-[30px]">
@@ -69,32 +73,49 @@ const Footer = (props: FooterProps) => {
       </div>
 
       <div className="ml-auto flex">
-        <Link href={"/mint"} >
-          <div className="hover-btn-shadow flex h-[64px] w-[200px] items-center justify-center rounded-[12px] border-2 border-black bg-white pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)] 4xl:h-[72px] 4xl:w-[260px]">
-            <span className="text-[21px] font-semibold leading-[21px] text-black 4xl:text-[24px] 4xl:leading-[24px]">
-              {t("check_whitelist")}
-            </span>
-          </div>
-        </Link>
-
-        <div className="hover-btn-shadow ml-[20px] flex h-[64px] w-[200px] flex-col items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)] 4xl:ml-[30px] 4xl:h-[72px] 4xl:w-[260px]">
-          <span className="text-[21px] font-semibold leading-[21px] text-black 4xl:text-[24px] 4xl:leading-[24px]">
-            {t("mint")}
-          </span>
-          {(props.countdown.days !== 0 ||
-            props.countdown.hours !== 0 ||
-            props.countdown.minutes !== 0 ||
-            props.countdown.seconds !== 0) && (
-              <span className="mt-[5px] text-[16px] font-semibold leading-[16px] text-black 4xl:text-[18px] 4xl:leading-[18px]">
-                {t("count_down", {
-                  day: props.countdown.days,
-                  hour: props.countdown.hours,
-                  minute: props.countdown.minutes,
-                  second: props.countdown.seconds,
-                })}
+        {
+          mintStartCountdown > 0 &&
+          <Link href={"/mint"} >
+            <div className="hover-btn-shadow flex h-[64px] w-[200px] items-center justify-center rounded-[12px] border-2 border-black bg-white pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)] 4xl:h-[72px] 4xl:w-[260px]">
+              <span className="text-[21px] font-semibold leading-[21px] text-black 4xl:text-[24px] 4xl:leading-[24px]">
+                {t("check_whitelist")}
               </span>
-            )}
-        </div>
+            </div>
+          </Link>
+        }
+
+        {
+          mintStartCountdown > 0 &&
+          <div className="hover-btn-shadow ml-[20px] flex h-[64px] w-[200px] flex-col items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)] 4xl:ml-[30px] 4xl:h-[72px] 4xl:w-[260px]">
+            <span className="text-[21px] font-semibold leading-[21px] text-black 4xl:text-[24px] 4xl:leading-[24px]">
+              {t("mint")}
+            </span>
+            {(props.countdown.days !== 0 ||
+              props.countdown.hours !== 0 ||
+              props.countdown.minutes !== 0 ||
+              props.countdown.seconds !== 0) && (
+                <span className="mt-[5px] text-[16px] font-semibold leading-[16px] text-black 4xl:text-[18px] 4xl:leading-[18px]">
+                  {t("count_down", {
+                    day: props.countdown.days,
+                    hour: props.countdown.hours,
+                    minute: props.countdown.minutes,
+                    second: props.countdown.seconds,
+                  })}
+                </span>
+              )}
+          </div>
+        }
+
+        {
+          mintStartCountdown <= 0 &&
+          <Link href={"/mint"}>
+            <div className="hover-btn-shadow ml-[20px] flex h-[64px] w-[200px] flex-col items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] pl-[5px] shadow-[4px_4px_0px_rgba(0,0,0,1)] 4xl:ml-[30px] 4xl:h-[72px] 4xl:w-[260px]">
+              <span className="text-[21px] font-semibold leading-[21px] text-black 4xl:text-[24px] 4xl:leading-[24px]">
+                {t("mint")}
+              </span>
+            </div>
+          </Link>
+        }
       </div>
     </div>
   );

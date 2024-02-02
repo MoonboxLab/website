748fbe551d2e5e61d58ac41164ebafc1d5f42767
D-lyw <liuyuanwang4321@gmail.com>
2024-02-02 10:23:36 +0800

    chore: stop listen mint user

--- app/[locale]/mint/MintUser.tsx
+++ app/[locale]/mint/MintUser.tsx
@@ -84,27 +84,27 @@ export default function MintUser(props: MintUserProps) {
     }
   }, [listenAddress])
 
-  useContractEvent({
-    ...NOBODY_CONTRACT_INFO,
-    eventName: 'PublicReserved',
-    listener: (logs) => {
-      console.log(logs[0].args.reserver)
-      if (logs[0].args.reserver) {
-        setListenAddress(logs[0].args.reserver)
-      }
-    }
-  })
+  // useContractEvent({
+  //   ...NOBODY_CONTRACT_INFO,
+  //   eventName: 'PublicReserved',
+  //   listener: (logs) => {
+  //     console.log(logs[0].args.reserver)
+  //     if (logs[0].args.reserver) {
+  //       setListenAddress(logs[0].args.reserver)
+  //     }
+  //   }
+  // })
 
-  useContractEvent({
-    ...NOBODY_CONTRACT_INFO,
-    eventName: 'WhitelistReserved',
-    listener: (logs) => {
-      console.log(logs[0].args.reserver)
-      if (logs[0].args.reserver) {
-        setListenAddress(logs[0].args.reserver)
-      }
-    }
-  })
+  // useContractEvent({
+  //   ...NOBODY_CONTRACT_INFO,
+  //   eventName: 'WhitelistReserved',
+  //   listener: (logs) => {
+  //     console.log(logs[0].args.reserver)
+  //     if (logs[0].args.reserver) {
+  //       setListenAddress(logs[0].args.reserver)
+  //     }
+  //   }
+  // })
 
   return <div className="w-full xl:w-[420px] sm:h-[390px] sm:p-[30px] sm:rounded-[24px] sm:border-black sm:border-[3px] sm:bg-white sm:shadow-[4px_4px_0px_rgba(0,0,0,1)] mt-[20px] sm:mt-0 ">
     <h3 className="text-white sm:text-black text-[21px] sm:text-[24px] font-bold leading-6 mb-[20px] mt-[30px] sm:mt-0"

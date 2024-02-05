8541d657fea6fe8b520e8b8f6cd122bee482194e
D-lyw <liuyuanwang4321@gmail.com>
2024-02-05 11:06:11 +0800

    chore: update format header font size

--- components/CustomConnectWallet.tsx
+++ components/CustomConnectWallet.tsx
@@ -43,18 +43,18 @@ const CustomConnectButton = () => {
             {(() => {
               if (!connected) {
                 return (
-                  <button className='hover-btn-shadow mt-[20px] flex h-[56px] w-full items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] shadow-[4px_4px_0px_rgba(0,0,0,1)] font-semibold' onClick={openConnectModal} type="button">
+                  <button className='hover-btn-shadow mt-[20px] flex h-[56px] w-full items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] shadow-[4px_4px_0px_rgba(0,0,0,1)] font-semibold text-[21px]' onClick={openConnectModal} type="button">
                     {t("header_connect_wallet")}
                   </button>
                 );
               }
               return (
                 <button
-                  className='hover-btn-shadow mt-[20px] flex h-[56px] w-full items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] shadow-[4px_4px_0px_rgba(0,0,0,1)] font-semibold'
+                  className='hover-btn-shadow mt-[20px] flex h-[56px] w-full items-center justify-center rounded-[12px] border-2 border-black bg-[#FFD600] shadow-[4px_4px_0px_rgba(0,0,0,1)] font-semibold text-[21px]'
                   onClick={() => disconnect()}
                   type="button"
                 >
-                  <Power className=' mr-3 text-black' /> {t("disconnect")}
+                  <Power className=' mr-3 text-black font-semibold' /> {t("disconnect")}
                 </button>
               );
             })()}

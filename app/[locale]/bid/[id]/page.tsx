20849a163bc1cfc85f2e6fa15ce39f268fae723d
Charles <hi@charlesify.com>
2024-10-08 20:32:45 +0800

    feat: Updated decimals

--- app/[locale]/bid/[id]/page.tsx
+++ app/[locale]/bid/[id]/page.tsx
@@ -84,127 +84,133 @@ export default function BidDetailsPage({ params }: { params: { id: string } }) {
                         </div>
                       )}
                     </div>
-                    <div className="mt-3 text-sm text-[#117E8A] lg:mt-5">
-                      {t("step1")}
-                    </div>
-                    <div className="flex items-baseline justify-between gap-2">
-                      <div className="text-lg font-bold lg:text-2xl">
-                        {t("enterNFT")}
+                    <>
+                      <div className="mt-3 text-sm text-[#117E8A] lg:mt-5">
+                        {t("step1")}
                       </div>
-                      <div className="text-xs font-normal lg:text-base">
-                        2d 1 h 53m | 24 {t("bids")}
+                      <div className="flex items-baseline justify-between gap-2">
+                        <div className="text-lg font-bold lg:text-2xl">
+                          {t("enterNFT")}
+                        </div>
+                        <div className="text-xs font-normal lg:text-base">
+                          2d 1 h 53m | 24 {t("bids")}
+                        </div>
                       </div>
-                    </div>
-                    <div className="mt-3 flex max-w-[300px] items-center gap-2 rounded border border-[#605D5E] p-3 text-sm lg:text-base">
-                      <div>Nobody #</div>
-                      <InputOTP maxLength={4}>
-                        <InputOTPGroup className="gap-2">
-                          <InputOTPSlot
-                            index={0}
-                            className="h-6 w-3 rounded-none border-0 border-y-0 border-b border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
-                          />
-                          <InputOTPSlot
-                            index={1}
-                            className="h-6 w-3 rounded-none border-0 border-y-0 border-b border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
-                          />
-                          <InputOTPSlot
-                            index={2}
-                            className="h-6 w-3 rounded-none border-0 border-y-0 border-b border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
-                          />
-                          <InputOTPSlot
-                            index={3}
-                            className="h-6 w-3 rounded-none border-0 border-y-0 border-b border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
-                          />
-                        </InputOTPGroup>
-                      </InputOTP>
-                    </div>
+                      <div className="mt-3 flex max-w-[300px] items-center gap-2 rounded border border-[#605D5E] p-3 text-sm lg:text-base">
+                        <div>Nobody #</div>
+                        <InputOTP maxLength={4}>
+                          <InputOTPGroup className="gap-2">
+                            <InputOTPSlot
+                              index={0}
+                              className="h-6 w-3 rounded-none border-0 border-y-0 border-b-2 border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
+                            />
+                            <InputOTPSlot
+                              index={1}
+                              className="h-6 w-3 rounded-none border-0 border-y-0 border-b-2 border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
+                            />
+                            <InputOTPSlot
+                              index={2}
+                              className="h-6 w-3 rounded-none border-0 border-y-0 border-b-2 border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
+                            />
+                            <InputOTPSlot
+                              index={3}
+                              className="h-6 w-3 rounded-none border-0 border-y-0 border-b-2 border-black first:rounded-l-none first:border-l-0 last:rounded-r-none last:border-r-0"
+                            />
+                          </InputOTPGroup>
+                        </InputOTP>
+                      </div>
 
-                    <div className="mt-3 text-sm text-[#117E8A] lg:mt-5">
-                      {t("step2")}
-                    </div>
-                    <div className="text-lg font-bold lg:text-2xl">
-                      {t("placeYourBid")}
-                    </div>
-                    <div className="mt-3 flex flex-col items-center gap-3 text-sm lg:mt-5 lg:flex-row lg:gap-2 lg:text-base">
-                      {steps.map((item) => (
-                        <button
-                          key={item}
-                          className="w-full rounded bg-[#117E8A] px-2 py-3 text-white lg:px-4 lg:py-2"
-                          onClick={() => {
-                            setPrice((currentBid + item).toString());
-                          }}
-                        >
-                          {t("bid")} USDT {currentBid + item}
-                        </button>
-                      ))}
-                    </div>
-                    <div className="mt-2 flex items-center gap-2 text-base uppercase lg:mt-5">
-                      <div className="h-px flex-1 bg-[#605D5E]/50"></div>
-                      <div>{t("or")}</div>
-                      <div className="h-px flex-1 bg-[#605D5E]/50"></div>
-                    </div>
-                    <div className="mt-2 text-sm font-medium lg:mt-3 lg:text-base">
-                      {t("maxBid")}
-                    </div>
-                    <div className="text-sm text-[#212427]/50 lg:text-base">
-                      {t("enterMore")}
-                    </div>
-                    <div className="flex flex-col gap-2 lg:flex-row">
-                      <div className="flex flex-1 gap-2 rounded border border-[#605D5E] p-3">
-                        <div>USDT</div>
-                        <input
-                          min={0}
-                          className="flex-1 bg-transparent outline-none"
-                          onInput={(e) => {
-                            let value = (e.target as HTMLInputElement).value;
-                            if (!/^[0-9]*\.?[0-9]*$/.test(value)) {
-                              value = value.replace(/[^0-9.]/g, ""); // 移除非数字和小数点的字符
-                              let index = -1;
-                              let find = false;
-                              value.split("").forEach((item, i) => {
-                                const char = price[i];
-                                if (item === "." && !find) {
-                                  index++;
-                                  if (char !== item) {
-                                    find = true;
-                                  }
-                                }
-                              });
-                              const parts = value.split(".");
-                              if (parts.length > 1) {
-                                value = parts
-                                  .map((item, i) => {
-                                    if (index === i) {
-                                      return item + ".";
-                                    }
-                                    return item;
-                                  })
-                                  .join("");
-                                value = value.replace(/^0+/, "");
-                              }
-                              (e.target as HTMLInputElement).value = value;
-                            }
-                            setPrice(value);
-                          }}
-                          value={price.toString()}
-                        />
+                      <div className="mt-3 text-sm text-[#117E8A] lg:mt-5">
+                        {t("step2")}
                       </div>
-                      <button className="rounded bg-[#117E8A] px-2 py-3 text-white lg:px-8 lg:py-2">
-                        {t("placeBid")}
-                      </button>
-                    </div>
-                    <div className="mt-2 text-xs text-[#212427] lg:mt-3 lg:text-sm">
-                      {t("bidDesc")}
-                    </div>
+                      <div className="text-lg font-bold lg:text-2xl">
+                        {t("placeYourBid")}
+                      </div>
+                      <div className="mt-3 flex flex-col items-center gap-3 text-sm lg:mt-5 lg:flex-row lg:gap-2 lg:text-base">
+                        {steps.map((item) => (
+                          <button
+                            key={item}
+                            className="w-full rounded bg-[#117E8A] px-2 py-3 text-white lg:px-4 lg:py-2"
+                            onClick={() => {
+                              setPrice((currentBid + item).toString());
+                            }}
+                          >
+                            {t("bid")} USDT {currentBid + item}
+                          </button>
+                        ))}
+                      </div>
+                      <div className="mt-2 flex items-center gap-2 text-base uppercase lg:mt-5">
+                        <div className="h-px flex-1 bg-[#605D5E]/50"></div>
+                        <div>{t("or")}</div>
+                        <div className="h-px flex-1 bg-[#605D5E]/50"></div>
+                      </div>
+                      <div className="mt-2 text-sm font-medium lg:mt-3 lg:text-base">
+                        {t("maxBid")}
+                      </div>
+                      <div className="text-sm text-[#212427]/50 lg:text-base">
+                        {t("enterMore")}
+                      </div>
+                      <div className="flex flex-col gap-2 lg:flex-row">
+                        <div className="flex flex-1 gap-2 rounded border border-[#605D5E] p-3">
+                          <div>USDT</div>
+                          <input
+                            min={0}
+                            className="flex-1 bg-transparent outline-none"
+                            onInput={(e) => {
+                              let value = (e.target as HTMLInputElement).value;
+                              if (!/^[0-9]*\.?[0-9]*$/.test(value)) {
+                                value = value.replace(/[^0-9.]/g, ""); // 移除非数字和小数点的字符
+                                let index = -1;
+                                let find = false;
+                                value.split("").forEach((item, i) => {
+                                  const char = price[i];
+                                  if (item === "." && !find) {
+                                    index++;
+                                    if (char !== item) {
+                                      find = true;
+                                    }
+                                  }
+                                });
+                                const parts = value.split(".");
+                                if (parts.length > 1) {
+                                  value = parts
+                                    .map((item, i) => {
+                                      if (index === i) {
+                                        return item + ".";
+                                      }
+                                      return item;
+                                    })
+                                    .join("");
+                                  value = value.replace(/^0+/, "");
+                                }
+                                (e.target as HTMLInputElement).value = value;
+                              }
+                              if (value.split(".").length > 1) {
+                                value =
+                                  (value.split(".")[0] || 0) +
+                                  "." +
+                                  value.split(".")[1].slice(0, 8);
+                              }
+                              setPrice(value);
+                            }}
+                            value={price.toString()}
+                          />
+                        </div>
+                        <button className="rounded bg-[#117E8A] px-2 py-3 text-white lg:px-8 lg:py-2">
+                          {t("placeBid")}
+                        </button>
+                      </div>
+                      <div className="mt-2 text-xs text-[#212427] lg:mt-3 lg:text-sm">
+                        {t("bidDesc")}
+                      </div>
+                    </>
                   </div>
                 </div>
                 <div className="mt-8 rounded border border-black/50 p-4 lg:mt-12 lg:p-8">
                   <div className="text-lg font-bold lg:text-2xl">
                     {t("itemSpec")}
                   </div>
-                  <div className="mt-2 text-sm lg:text-base">
-                    {t("itemSpecDesc")}
-                  </div>
+                  <div className="mt-2 text-sm lg:text-base"></div>
                 </div>
               </>
             ) : (
